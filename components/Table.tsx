fbb7dced41eaa1e5fe98b0b624f3c340acab9771
rakesh <rakeshbeesetty53@gmail.com>
2024-09-10 14:25:32 -0700

    Created Table component and cleaned the page.tsx file.

---
+++ components/Table.tsx
@@ -0,0 +1,19 @@
+const Table = () => {
+    return(
+        <>
+            <table>
+                <thead>
+                <tr>
+                    <th>Branch</th>
+                    <th>Date</th>
+                    <th>No. of Hours</th>
+                    <th>Fuel Price</th>
+                    <th>Fuel consumption per Hour</th>
+                </tr>
+                </thead>
+            </table>
+        </>
+    )
+}
+
+export default Table;
