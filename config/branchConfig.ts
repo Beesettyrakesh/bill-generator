b8aa3cbd56c533a42156016877a4bfb6ea4dea0b
rakesh <rakeshbeesetty53@gmail.com>
2024-09-11 14:15:48 -0700

    Added a new [key,value] pair.

--- config/branchConfig.ts
+++ config/branchConfig.ts
@@ -1,32 +1,22 @@
 export const branchConfig= {
+  "Seethammadhara": {
+    "company": "Godavari Power Systems",
+    "template": "HOURS",
+    "genSetCapacity": "62.5 KVA"
+  },
   "Chinamushidiwada" : {
     "company": "Godavari Power Systems",
     "template": "HOURS",
     "genSetCapacity": "50 KVA"
-  },
-  "Gajuwaka" : {
-    "company": "Godavari Power Systems",
-    "template": "START_AND_END",
-    "genSetCapacity": "62.5 KVA"
-  },
-  "Industrial Estate": {
-    "company": "SRI SAI SABITHA ELECTRICALS",
-    "template": "HOURS",
-    "genSetCapacity": "62.5 KVA"
   },
   "Kakani Nagar": {
     "company": "Godavari Power Systems",
     "template": "START_AND_END",
     "genSetCapacity": "20 KVA"
   },
-  "Nakkapalli": {
-    "company": "SRI SAI SABITHA ELECTRICALS",
-    "template": "HOURS",
-    "genSetCapacity": "20 KVA"
-  },
-  "Seethammadhara": {
+  "Gajuwaka" : {
     "company": "Godavari Power Systems",
-    "template": "HOURS",
+    "template": "START_AND_END",
     "genSetCapacity": "62.5 KVA"
   },
   "Srinagar(Gajuwaka)": {
@@ -34,14 +24,24 @@ export const branchConfig= {
     "template": "HOURS",
     "genSetCapacity": "35 KVA"
   },
+  "Yellamanchilli": {
+    "company": "Godavari Power Systems",
+    "template": "HOURS",
+    "genSetCapacity": "40 KVA"
+  },
+  "Industrial Estate": {
+    "company": "SRI SAI SABITHA ELECTRICALS",
+    "template": "HOURS",
+    "genSetCapacity": "62.5 KVA"
+  },
   "Vishalakshi Nagar": {
     "company": "SRI SAI SABITHA ELECTRICALS",
     "template": "HOURS",
     "genSetCapacity": "15 KVA"
   },
-  "Yellamanchilli": {
-    "company": "Godavari Power Systems",
+  "Nakkapalli": {
+    "company": "SRI SAI SABITHA ELECTRICALS",
     "template": "HOURS",
-    "genSetCapacity": "40 KVA"
-  }
+    "genSetCapacity": "20 KVA"
+  },
 }
