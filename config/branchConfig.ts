b3d3a73edcbd066b0894af16be27337cc7c0d799
rakesh <rakeshbeesetty53@gmail.com>
2024-09-11 12:55:21 -0700

    Added config files.

---
+++ config/branchConfig.ts
@@ -0,0 +1,47 @@
+export const branchConfig= {
+  "Chinamushidiwada" : {
+    "company": "Godavari Power Systems",
+    "template": "HOURS",
+    "genSetCapacity": "50 KVA"
+  },
+  "Gajuwaka" : {
+    "company": "Godavari Power Systems",
+    "template": "START_AND_END",
+    "genSetCapacity": "62.5 KVA"
+  },
+  "Industrial Estate": {
+    "company": "SRI SAI SABITHA ELECTRICALS",
+    "template": "HOURS",
+    "genSetCapacity": "62.5 KVA"
+  },
+  "Kakani Nagar": {
+    "company": "Godavari Power Systems",
+    "template": "START_AND_END",
+    "genSetCapacity": "20 KVA"
+  },
+  "Nakkapalli": {
+    "company": "SRI SAI SABITHA ELECTRICALS",
+    "template": "HOURS",
+    "genSetCapacity": "20 KVA"
+  },
+  "Seethammadhara": {
+    "company": "Godavari Power Systems",
+    "template": "HOURS",
+    "genSetCapacity": "62.5 KVA"
+  },
+  "Srinagar(Gajuwaka)": {
+    "company": "Godavari Power Systems",
+    "template": "HOURS",
+    "genSetCapacity": "35 KVA"
+  },
+  "Vishalakshi Nagar": {
+    "company": "SRI SAI SABITHA ELECTRICALS",
+    "template": "HOURS",
+    "genSetCapacity": "15 KVA"
+  },
+  "Yellamanchilli": {
+    "company": "Godavari Power Systems",
+    "template": "HOURS",
+    "genSetCapacity": "40 KVA"
+  }
+}
---
+++ config/companyConfig.ts
@@ -0,0 +1,12 @@
+export const companyConfig = {
+  "Godavari Power Systems": {
+    "address": "DOOR NO: 39-5-16/15, SECTOR-5, NEAR TENNIS COURT, MURALINAGAR, VISAKHAPATNAM-530007",
+    "contact": "7997595559 & 8367041904",
+    "account": "35113298330 Vender ID GODA 2623849 with SBI, INDUSTRIAL ESTATE BRANCH"
+  },
+  "Sri Sai Sabitha Electricals": {
+    "address": "D.No: 39-5-16/15, Krishna Sai Nivas-1, Near Tennis Court, Muralinagar Visakhapatnam-7",
+    "contact": "7997595559, 6304672804",
+    "account": "30101952601 with SBI, Sriharipuram Branch"
+  }
+}
