186072956b10364212f1abc0ce9ba5f54e014bc3
rakesh <rakeshbeesetty53@gmail.com>
2024-09-11 14:19:35 -0700

    Corrected the spelling mistakes in address property

--- config/companyConfig.ts
+++ config/companyConfig.ts
@@ -2,7 +2,7 @@ export const companyConfig = {
   "Godavari Power Systems": {
     "address": "DOOR NO: 39-5-16/15, SECTOR-5, NEAR TENNIS COURT, MURALINAGAR, VISAKHAPATNAM-530007",
     "contact": "7997595559 & 8367041904",
-    "account": "35113298330 Vender ID GODA 2623849 with SBI, INDUSTRIAL ESTATE BRANCH"
+    "account": "35113298330 Vendor ID GODA 2623849 with SBI, INDUSTRIAL ESTATE BRANCH"
   },
   "Sri Sai Sabitha Electricals": {
     "address": "D.No: 39-5-16/15, Krishna Sai Nivas-1, Near Tennis Court, Muralinagar Visakhapatnam-7",
