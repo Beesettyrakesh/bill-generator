ec65566806160aa4de741010c762081180f90578
rakesh <rakeshbeesetty53@gmail.com>
2024-09-13 21:33:37 -0700

    Added input fields for each row.
    Preserves the state value of each input field for passing the values to the generateDocument function.

--- components/TableRow.tsx
+++ components/TableRow.tsx
@@ -1,29 +1,98 @@
+'use client'
 import "../css/TableRow.css"
-import {BankBranchInterface} from "@/interfaces/BankBranchInterface";
+import {IBankBranch} from "@/interfaces/IBankBranch";
+import {useEffect, useState} from "react";
+import generateDocument from "@/utils/generateDocument";
+import calculateTotal from "@/utils/calculateTotal";
 
 interface TableRowProps{
-    branch: BankBranchInterface
+    branch: IBankBranch
 }
 const TableRow = (props: TableRowProps) => {
+    const [branch] = useState(props.branch.name)
+    const [date, setDate] = useState("");
+    const [startReading, setStartReading] = useState("");
+    const [endReading, setEndReading] = useState("");
+    const [hours, setHours] = useState("");
+    const [fuelPrice, setFuelPrice] = useState("");
+    const [total, setTotal] = useState("");
+
+    const reset = () => {
+        setDate("")
+        setStartReading("")
+        setEndReading("")
+        setHours("")
+        setFuelPrice("")
+    }
+
+    useEffect(() => {
+        if (hours && fuelPrice && branch){
+            const billTotal: number = calculateTotal(Number(hours), Number(fuelPrice), branch)
+            setTotal( billTotal.toFixed(2) )
+        }
+    }, [hours, fuelPrice, branch]);
+
     return(
         <>
             <tr className="bank-row">
                 <td>{props.branch.name}</td>
-                <td><input type="date"/></td>
+                <td>
+                    <input type="date"
+                           value={date}
+                           onChange={(event) => setDate(event.target.value)}
+                    />
+                </td>
                 {
                     props.branch.template === "START_AND_END"
-                        ? <td><input type="text" placeholder="Enter start reading"/></td>
+                        ? <td>
+                            <input type="text"
+                                   placeholder="Enter start reading"
+                                   value={startReading}
+                                   onChange={(event) => setStartReading(event.target.value)}
+                            />
+                          </td>
                         : <td></td>
                 }
                 {
                     props.branch.template === "START_AND_END"
-                        ? <td><input type="text" placeholder="Enter end reading"/></td>
+                        ? <td>
+                            <input type="text"
+                                   placeholder="Enter end reading" value={endReading}
+                                   onChange={(event) => setEndReading(event.target.value)}
+                            />
+                          </td>
                         : <td></td>
                 }
-                <td><input type="text" placeholder="Enter no. of hrs"/></td>
-                <td><input type="text" placeholder="Enter fuel price"/></td>
-                <td><input type="text" placeholder="fuel/hr"/></td>
-                <td>{0.00}</td>
+                <td>
+                    <input type="text"
+                           placeholder="Enter no. of hrs"
+                           value={hours}
+                           onChange={(event) => setHours(event.target.value)}
+                    />
+                </td>
+                <td>
+                    <input type="text"
+                           placeholder="Enter fuel price" value={fuelPrice}
+                           onChange={(event) => setFuelPrice(event.target.value)}
+                    />
+                </td>
+                <td>
+                    {total}
+                </td>
+                <td>
+                    <button onClick={() => generateDocument({
+                        branch,
+                        date,
+                        startReading,
+                        endReading,
+                        hours,
+                        fuelPrice,
+                        total
+                    })}>Generate Bill</button>
+                </td>
+                <td>
+                    <button onClick={reset}>Reset</button>
+                </td>
             </tr>
         </>
     )
