e7969d8c88ab7b30aac4530e1f9ef41ef0432906
rakesh <rakeshbeesetty53@gmail.com>
2024-09-13 21:14:25 -0700

    Key property of each element is changed to Upper-case

--- config/companyConfig.ts
+++ config/companyConfig.ts
@@ -1,10 +1,10 @@
 export const companyConfig = {
-  "Godavari Power Systems": {
+  "GODAVARI POWER SYSTEMS": {
     "address": "DOOR NO: 39-5-16/15, SECTOR-5, NEAR TENNIS COURT, MURALINAGAR, VISAKHAPATNAM-530007",
     "contact": "7997595559 & 8367041904",
     "account": "35113298330 Vendor ID GODA 2623849 with SBI, INDUSTRIAL ESTATE BRANCH"
   },
-  "Sri Sai Sabitha Electricals": {
+  "SRI SAI SABITHA ELECTRICALS": {
     "address": "D.No: 39-5-16/15, Krishna Sai Nivas-1, Near Tennis Court, Muralinagar Visakhapatnam-7",
     "contact": "7997595559, 6304672804",
     "account": "30101952601 with SBI, Sriharipuram Branch"
