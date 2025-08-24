393ee71301165454234ef56e7d5c2f78e140d124
Beesettyrakesh <rakeshbeesetty53@gmail.com>
2025-08-24 11:28:39 +0530

    Changed the icon to use electric-generator.png

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
-import ClientSessionProvider from './client-session-provider';
+import ClientSessionProvider from "./client-session-provider";
 
 export const metadata = {
-  title: 'Billezy',
-  description: 'Generate and manage bills for different branches',
-  keywords: ['bill generator', 'invoice generator', 'document management'],
+  title: "Billezy",
+  description: "Generate and manage bills for different branches",
+  keywords: ["bill generator", "invoice generator", "document management"],
   icons: {
-    icon: '/generator.png',
-    apple: '/generator.png',
+    icon: "/electric-generator.png",
+    apple: "/electric-generator.png",
   },
 };
 
